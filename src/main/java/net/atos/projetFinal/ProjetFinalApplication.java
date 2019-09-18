6df375364f6691ecb0b7ee9064d40e8776abc9ed
sum <javaid.sumaira@gmail.com>
2019-09-18 14:28:30 +0200

    Initial commit

---
+++ src/main/java/net/atos/projetFinal/ProjetFinalApplication.java
@@ -0,0 +1,13 @@
+package net.atos.projetFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetFinalApplication.class, args);
+	}
+
+}
---
+++ src/main/java/net/atos/projetFinal/model/package-info.java
@@ -0,0 +1,1 @@
+package net.atos.projetFinal.model;
