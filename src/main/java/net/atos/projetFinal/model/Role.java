323b27d8aebedf5a3e3380822f08c89790862999
Nils <nils.vovan@gmail.com>
2019-09-19 09:53:51 +0200

    Ajout de commentaire pour les 2 bean + ajout getter and setters pour employe

--- src/main/java/net/atos/projetFinal/model/Role.java
+++ src/main/java/net/atos/projetFinal/model/Role.java
@@ -1,5 +1,7 @@
 package net.atos.projetFinal.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,26 +9,38 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+ * Description des différents Role possible pour le employés
+ * Entity de la table Role
+ * @author NVV
+ *
+ */
 @Entity
 @Table(name = "Role")
-public class Role {
+public class Role implements Serializable{
+	
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Id qui point sur la primary key de la table role qui est programmée en auto incremental
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name="idRole")
-	int id ;
+	private int id ;
 	
+	/**
+	 * nom du rôle pointe sur la colonne nomRole de la table
+	 */
 	@Column(name="nomRole", length = 75)
-	String nom ;
+	private String nom ;
 
 	public Role() {
 		super();
-	}
-
-	public Role(int id, String nom) {
-		super();
-		this.id = id;
-		this.nom = nom;
 	}
 
 	public int getId() {
