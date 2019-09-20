d584b8cfdd3b282983c751009c1f0aab32ddcccc
Nils <nils.vovan@gmail.com>
2019-09-20 12:06:59 +0200

    Changer OneToOne en ManyToOne pour role

--- src/main/java/net/atos/projetFinal/model/Employe.java
+++ src/main/java/net/atos/projetFinal/model/Employe.java
@@ -2,18 +2,15 @@ package net.atos.projetFinal.model;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.List;
 
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -48,7 +45,7 @@ public class Employe implements Serializable {
 	@Basic
 	private LocalDateTime createTime;
 
-	@OneToOne
+	@ManyToOne
 	@JoinColumn(name = "Role_idRole")
 	private Role role;
 
